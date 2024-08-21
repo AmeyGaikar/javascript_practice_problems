58f105de223e66c227aac12e757e43ffd39620d8
Amey Gaikar <gaikaramey04@gmail.com>
2024-08-21 18:56:51 +0530

    Solve finding greatest number in an array.

--- greatest_num_in_array.js
+++ greatest_num_in_array.js
@@ -1,11 +1,13 @@
 function largestNum(numArr) {
- let largestNum = numArr[0];
-
-    numArr.forEach(num => {
-        if(num > largestNum){
-            largestNum = num;
-        }
-    });
-}
-
-console.log(largestNum([1,2,3,4,5,6,7,8,9]));
+    let largestNum = 1;
+   
+       numArr.forEach(num => {
+           if(num > largestNum){
+               largestNum = num
+           }
+           
+       });
+   return largestNum;
+   }
+   
+   console.log(largestNum([0,100,4]));
